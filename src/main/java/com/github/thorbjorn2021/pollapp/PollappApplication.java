4e211d85d67d6a1c62e8c538b32a632b7ad796e3
Thorbjørn Melheim <thomelheim@gmail.com>
2024-09-03 22:51:31 +0200

    First commit

---
+++ src/main/java/com/github/thorbjorn2021/pollapp/PollappApplication.java
@@ -0,0 +1,13 @@
+package com.github.thorbjorn2021.pollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/thorbjorn2021/pollapp/PollappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thorbjorn2021.pollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
