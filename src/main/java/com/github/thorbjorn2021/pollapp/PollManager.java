0006ae184933366f25bb5e690f950ac963d3e888
Thorbjørn Melheim <thomelheim@gmail.com>
2024-09-04 11:00:46 +0200

    Added domain classes

---
+++ src/main/java/com/github/thorbjorn2021/pollapp/PollManager.java
@@ -0,0 +1,7 @@
+package com.github.thorbjorn2021.pollapp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PollManager {
+}
---
+++ src/main/java/com/github/thorbjorn2021/pollapp/VoteOption.java
@@ -0,0 +1,6 @@
+package com.github.thorbjorn2021.pollapp;
+
+public class VoteOption {
+    private String caption;
+    private int presentationOrder;
+}
