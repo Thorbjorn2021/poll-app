0ff8eb576387fd44c19f6aba515504e52015a24a
Thorbjørn Melheim <thomelheim@gmail.com>
2024-09-05 22:20:50 +0200

    Added properties

--- src/main/java/com/github/thorbjorn2021/pollapp/PollManager.java
+++ src/main/java/com/github/thorbjorn2021/pollapp/PollManager.java
@@ -2,6 +2,14 @@ package com.github.thorbjorn2021.pollapp;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class PollManager {
+    public Map<Long, User> users = new HashMap<Long, User>();
+    public Map<Long, Poll> polls = new HashMap<Long, Poll>();
+
+    public PollManager() {
+    }
 }
