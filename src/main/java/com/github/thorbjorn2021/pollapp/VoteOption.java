bc65024a0cd9e1a44cb7c229a04b57ab4e503811
Thorbjørn Melheim <thomelheim@gmail.com>
2024-09-08 20:55:42 +0200

    Added constructors + getters and setters

--- src/main/java/com/github/thorbjorn2021/pollapp/VoteOption.java
+++ src/main/java/com/github/thorbjorn2021/pollapp/VoteOption.java
@@ -3,4 +3,25 @@ package com.github.thorbjorn2021.pollapp;
 public class VoteOption {
     private String caption;
     private int presentationOrder;
+
+    public VoteOption(String caption, int presentationOrder) {
+        this.caption = caption;
+        this.presentationOrder = presentationOrder;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public int getPresentationOrder() {
+        return presentationOrder;
+    }
+
+    public void setPresentationOrder(int presentationOrder) {
+        this.presentationOrder = presentationOrder;
+    }
 }
