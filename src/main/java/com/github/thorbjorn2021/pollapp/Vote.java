b0836d172cc261bd77e427da8f0fb142b610a93c
Thorbjørn Melheim <thomelheim@gmail.com>
2024-09-08 22:43:54 +0200

    Added new properties

--- src/main/java/com/github/thorbjorn2021/pollapp/Vote.java
+++ src/main/java/com/github/thorbjorn2021/pollapp/Vote.java
@@ -4,8 +4,39 @@ import java.time.Instant;
 
 public class Vote {
     private Instant publishedAt;
+    private Poll poll;
+    private User user;
+    private VoteOption voteOption;
 
-    public Vote() {
+    public Vote(Instant publishedAt, Poll poll, User user, VoteOption voteOption) {
+        this.publishedAt = publishedAt;
+        this.poll = poll;
+        this.user = user;
+        this.voteOption = voteOption;
+    }
+
+    public Poll getPoll() {
+        return poll;
+    }
+
+    public void setPoll(Poll poll) {
+        this.poll = poll;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public VoteOption getVoteOption() {
+        return voteOption;
+    }
+
+    public void setVoteOption(VoteOption voteOption) {
+        this.voteOption = voteOption;
     }
 
     public Instant getPublishedAt() {
