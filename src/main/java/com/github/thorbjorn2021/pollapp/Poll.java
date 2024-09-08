cb807b2add3ebb051cfb11915e6a22bf0655da51
Thorbjørn Melheim <thomelheim@gmail.com>
2024-09-08 17:25:03 +0200

    Added more properties

--- src/main/java/com/github/thorbjorn2021/pollapp/Poll.java
+++ src/main/java/com/github/thorbjorn2021/pollapp/Poll.java
@@ -1,13 +1,22 @@
 package com.github.thorbjorn2021.pollapp;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Poll {
     private String question;
     private Instant publishedAt;
     private Instant validUntil;
+    private Set<VoteOption> options;
+    private User creator;
 
-    public Poll() {
+    public Poll(String question, Instant publishedAt, Instant validUntil, User creator) {
+        this.question = question;
+        this.publishedAt = publishedAt;
+        this.validUntil = validUntil;
+        this.creator = creator;
+        options = new HashSet<>();
     }
 
     public String getQuestion() {
@@ -34,9 +43,19 @@ public class Poll {
         this.validUntil = validUntil;
     }
 
-    public Poll(String question, Instant publishedAt, Instant validUntil) {
-        this.question = question;
-        this.publishedAt = publishedAt;
-        this.validUntil = validUntil;
+    public Set<VoteOption> getOptions() {
+        return options;
+    }
+
+    public void setOptions(Set<VoteOption> options) {
+        this.options = options;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
     }
 }
